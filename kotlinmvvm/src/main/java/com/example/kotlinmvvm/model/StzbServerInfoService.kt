0fba481810f60e422523b0f57cd78685d88bbcdc
1985848358@qq.com <5942036asd>
2020-03-04 18:00:55 +0800

    完善详情页

--- kotlinmvvm/src/main/java/com/example/kotlinmvvm/model/StzbServerInfoService.kt
+++ kotlinmvvm/src/main/java/com/example/kotlinmvvm/model/StzbServerInfoService.kt
@@ -1,10 +1,17 @@
 package com.example.kotlinmvvm.model
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface StzbServerInfoService {
 
     @GET("server_list?_=1583307101540&callback=jsonp2")
     suspend fun getServerList()
+
+    @GET("city")
+    suspend fun getServerCityInfo(@Query("server_id")server_id:String,@Query("date")date:String,@Query("")test:String = "1583307291083",@Query("callback")callback:String)
+
+    @GET("allies_top_10")
+    suspend fun getServerRank(@Query("server_id")server_id:String,@Query("date")date:String,@Query("_")test:String = "1583308047151",@Query("callback")callback:String)
 
 }
--- kotlinmvvm/src/main/java/com/example/kotlinmvvm/util/ContactDecoration.kt
+++ kotlinmvvm/src/main/java/com/example/kotlinmvvm/util/ContactDecoration.kt
@@ -14,4 +14,5 @@ class ContactDecoration constructor() : ItemDecoration() {
     }
 
 
+
 }
