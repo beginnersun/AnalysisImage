51c2c53d71f31cdd2b3bd26b0184fa2fbdc60486
1985848358@qq.com <5942036asd>
2020-03-23 17:56:28 +0800

    Android10

--- kotlinmvvm/src/main/java/com/example/kotlinmvvm/widget/BitImageView.kt
+++ kotlinmvvm/src/main/java/com/example/kotlinmvvm/widget/BitImageView.kt
@@ -5,17 +5,17 @@ import android.graphics.*
 import android.net.Uri
 import android.util.AttributeSet
 import android.util.Log
+import android.util.LruCache
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
-import com.example.kotlinmvvm.R
+import com.example.base_module.util.BitmapUtil
 import java.io.InputStream
-import kotlin.math.max
 
 class BitImageView : View {
 
-    private var mDecoder:BitmapRegionDecoder? = null
+    private var mDecoder: BitmapRegionDecoder? = null
     private var mImageWidth = 0
     private var mImageHeight = 0
     private var mImageCurrentWidth = 0f   //截取图片的宽度
@@ -24,46 +24,52 @@ class BitImageView : View {
     private var oldImageHeight = 0f
     private var mScale = 1f
     private var mMatrix = Matrix()
-    @Volatile private var mRect = RectF()
-    private val options:BitmapFactory.Options = BitmapFactory.Options()
+    @Volatile
+    private var mRect = RectF()
+    private val options: BitmapFactory.Options = BitmapFactory.Options()
     private var gestureDetector: GestureDetector? = null
-    private var scaleGestureDetector:ScaleGestureDetector? = null
+    private var scaleGestureDetector: ScaleGestureDetector? = null
     private var maxScale = 5f
     private var minScale = 0.2f
+
+    private val blockSize = 200  //每 200 * 200 为一个块
+    private val drawRect = Rect() //当前View的绘制区域  （计算出当前区域能有几块blockSize）
+
+    private var imageCache: LruCache<Point, Bitmap>? = null
+
     init {
         options.inPreferredConfig = Bitmap.Config.RGB_565
     }
 
     private var startScroll = 0L
 
-    constructor(context: Context?) : this(context,null,0)
+    constructor(context: Context?) : this(context, null, 0)
 
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init()
         setData(context!!.assets.open("girl.jpg"))
-//        setData(R.mipmap.big_bg)
     }
 
-    private fun init(){
-        gestureDetector = GestureDetector(context,object :GestureDetector.SimpleOnGestureListener(){
+
+    private fun init() {
+        gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
 
             override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
                 startScroll = System.currentTimeMillis()
-                Log.e("开始滑动时间","$startScroll")
 //                Log.e("onScroll变化","$mImageCurrentWidth     $mImageWidth     $mImageCurrentHeight     $mImageHeight         $distanceX       $distanceY")
                 if (mImageCurrentWidth < mImageWidth) {
-                    mRect.left = mRect.left + distanceX/mScale
-                    mRect.right = mRect.right + distanceX/mScale
+                    mRect.left = mRect.left + distanceX / mScale
+                    mRect.right = mRect.right + distanceX / mScale
                     checkRectWidth()
                 }
-                if (mImageCurrentHeight < mImageHeight){
-                    mRect.top = mRect.top + distanceY/mScale
-                    mRect.bottom = mRect.bottom + distanceY/mScale
+                if (mImageCurrentHeight < mImageHeight) {
+                    mRect.top = mRect.top + distanceY / mScale
+                    mRect.bottom = mRect.bottom + distanceY / mScale
                     checkRectHeight()
                 }
-                if (mImageCurrentWidth < mImageWidth || mImageCurrentHeight < mImageHeight){
+                if (mImageCurrentWidth < mImageWidth || mImageCurrentHeight < mImageHeight) {
                     invalidate()
                 }
                 return super.onScroll(e1, e2, distanceX, distanceY)
@@ -75,128 +81,135 @@ class BitImageView : View {
 
         })
 
-        scaleGestureDetector = ScaleGestureDetector(context,object :ScaleGestureDetector.SimpleOnScaleGestureListener(){
-            override fun onScale(detector: ScaleGestureDetector?): Boolean {
-                var tmpScale = detector!!.scaleFactor
-                val newScale = mScale * tmpScale
-                Log.e("onScale缩放","$tmpScale    $mScale     $newScale     $maxScale    $minScale")
-                if (newScale > maxScale){
-                    tmpScale = maxScale/mScale
+        scaleGestureDetector =
+            ScaleGestureDetector(context, object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector?): Boolean {
+                    var tmpScale = detector!!.scaleFactor
+                    val newScale = mScale * tmpScale
+                    Log.e("onScale缩放", "$tmpScale    $mScale     $newScale     $maxScale    $minScale")
+                    if (newScale > maxScale) {
+                        tmpScale = maxScale / mScale
+                    }
+                    if (newScale < minScale) {
+                        tmpScale = minScale / mScale
+                    }
+                    mScale *= tmpScale
+                    updateScale(tmpScale)
+                    return super.onScale(detector)
                 }
-                if (newScale < minScale){
-                    tmpScale = minScale / mScale
+
+                override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+                    return true
                 }
-                mScale *=tmpScale
-                updateScale(tmpScale)
-                return super.onScale(detector)
+            })
+
+        imageCache = object : LruCache<Point, Bitmap>(5) {
+            override fun sizeOf(key: Point?, value: Bitmap?): Int =
+                BitmapUtil.getBitmapSize(value!!)
+
+            override fun create(key: Point?): Bitmap {
+                val rect = createBlockRect(key!!.x,key!!.y)
+                return super.create(key)
             }
 
-            override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
-                return true
-            }
-        })
+            private fun createBlockRect(x: Int, y: Int) =
+                Rect(x * blockSize, y * blockSize, (x + 1) * blockSize, (y + 1) * blockSize)
+
+        }
     }
 
-    private fun updateScale(tmpScale : Float){
+    private fun updateScale(tmpScale: Float) {
         oldImageWidth = mImageCurrentWidth
         oldImageHeight = mImageCurrentHeight
 
         mImageCurrentWidth /= tmpScale
         mImageCurrentHeight /= tmpScale
 
-        Log.e("缩放的值","$tmpScale    $mImageCurrentWidth    $mImageCurrentHeight")
-        mRect.left = mRect.left - (mImageCurrentWidth-oldImageWidth)/2   //当前状态减去要向左边延伸的长度
-        mRect.right = mRect.right + (mImageCurrentWidth-oldImageWidth)/2
-        mRect.top = mRect.top - (mImageCurrentHeight - oldImageHeight)/2
-        mRect.bottom = mRect.bottom + (mImageCurrentHeight - oldImageHeight)/2
+        mRect.left = mRect.left - (mImageCurrentWidth - oldImageWidth) / 2   //当前状态减去要向左边延伸的长度
+        mRect.right = mRect.right + (mImageCurrentWidth - oldImageWidth) / 2
+        mRect.top = mRect.top - (mImageCurrentHeight - oldImageHeight) / 2
+        mRect.bottom = mRect.bottom + (mImageCurrentHeight - oldImageHeight) / 2
 
         if (mImageCurrentWidth < mImageWidth) {
             checkRectWidth()
         }
-        if (mImageCurrentHeight < mImageHeight){
+        if (mImageCurrentHeight < mImageHeight) {
             checkRectHeight()
         }
-        Log.e("生成位置","${mRect.left}    ${mRect.right}    ${mRect.top}  ${mRect.bottom}  ")
         invalidate()
     }
 
-    private fun checkRectWidth(){
+    private fun checkRectWidth() {
 
-        if (mRect.left < 0){
+        if (mRect.left < 0) {
             mRect.left = 0f
             mRect.right = mRect.left + mImageCurrentWidth
         }
-        if (mRect.right > mImageWidth){
-            mRect.right = mImageWidth*1f
+        if (mRect.right > mImageWidth) {
+            mRect.right = mImageWidth * 1f
             mRect.left = mRect.right - mImageCurrentWidth
         }
     }
 
-    private fun checkRectHeight(){
+    private fun checkRectHeight() {
 
-        if (mRect.top < 0){
+        if (mRect.top < 0) {
             mRect.top = 0f
             mRect.bottom = mRect.top + mImageCurrentHeight
         }
 
-        if (mRect.bottom > mImageHeight){
-            mRect.bottom = mImageHeight*1f
+        if (mRect.bottom > mImageHeight) {
+            mRect.bottom = mImageHeight * 1f
             mRect.top = mRect.bottom - mImageCurrentHeight
         }
     }
 
 
-    fun setData(inputStream: InputStream){
-        mDecoder = BitmapRegionDecoder.newInstance(inputStream,false)
+    fun setData(inputStream: InputStream) {
+        mDecoder = BitmapRegionDecoder.newInstance(inputStream, false)
         val tmpOptions = BitmapFactory.Options()
         tmpOptions.inJustDecodeBounds = true
-        BitmapFactory.decodeStream(inputStream,null,tmpOptions)
+        BitmapFactory.decodeStream(inputStream, null, tmpOptions)
         mImageWidth = mDecoder!!.width
         mImageHeight = mDecoder!!.height
-        Log.e("生成图片大小","$mImageWidth    $mImageHeight")
-        matrix.setScale(mScale,mScale)
+        matrix.setScale(mScale, mScale)
         invalidate()
         inputStream.close()
+
     }
 
-    fun setData(uri: Uri){
+    fun setData(uri: Uri) {
         val inputStream = context.contentResolver.openInputStream(uri)
-        mDecoder = BitmapRegionDecoder.newInstance(inputStream,false)
+        mDecoder = BitmapRegionDecoder.newInstance(inputStream, false)
         val tmpOptions = BitmapFactory.Options()
         tmpOptions.inJustDecodeBounds = true
-        BitmapFactory.decodeStream(inputStream,null,tmpOptions)
+        BitmapFactory.decodeStream(inputStream, null, tmpOptions)
         mImageWidth = mDecoder!!.width
         mImageHeight = mDecoder!!.height
         invalidate()
         inputStream?.close()
     }
 
-    fun setData(resource:Int){
-        Log.e("路径","android.resource://${context.packageName}/${resource}")
-        setData(Uri.parse("android.resource://${context.packageName}/${resource}"))
-    }
-
     override fun onDraw(canvas: Canvas?) {
-        if(mDecoder!=null) {
+        getDrawingRect(drawRect)
+        if (mDecoder != null) {
             val startTime = System.currentTimeMillis()
-            Log.e("开始时间startTime","$startTime")
 //            Log.e("切割位置","${mRect.left}    ${mRect.right}    ${mRect.top}  ${mRect.bottom}     ${width}    ${height}")
-            val rect =  Rect().apply {
+            val rect = Rect().apply {
                 left = mRect.left.toInt()
                 right = mRect.right.toInt()
                 top = mRect.top.toInt()
                 bottom = mRect.bottom.toInt()
             }
+            mRect.isEmpty
             val bitmap = mDecoder!!.decodeRegion(rect, options)
-            mMatrix.setScale(mScale,mScale)
+            mMatrix.setScale(mScale, mScale)
             val middle = System.currentTimeMillis()
-            Log.e("绘制中间时间startTime","$middle      生成Bitmap 时间差值为${middle-startTime} ")
             canvas!!.drawBitmap(bitmap, mMatrix, Paint())
             val endTime = System.currentTimeMillis()
-            if(bitmap!=null && !bitmap.isRecycled) {
+            if (bitmap != null && !bitmap.isRecycled) {
                 bitmap.recycle()
             }
-            Log.e("绘制结束时间startTime","$endTime       时间差值为${endTime-startTime}    触发后的时间差${endTime - startScroll}")
         }
     }
 
@@ -211,20 +224,42 @@ class BitImageView : View {
         mImageCurrentWidth = measuredWidth.toFloat()
         mImageCurrentHeight = measuredHeight.toFloat()
 
-        if (maxScale != -1f && (mImageCurrentWidth != 0f && mImageCurrentHeight != 0f)){
-            maxScale = if (mImageWidth /mImageCurrentWidth > mImageHeight / mImageCurrentHeight) mImageWidth / mImageCurrentWidth else mImageHeight / mImageCurrentHeight
+        if (maxScale != -1f && (mImageCurrentWidth != 0f && mImageCurrentHeight != 0f)) {
+            maxScale =
+                if (mImageWidth / mImageCurrentWidth > mImageHeight / mImageCurrentHeight) mImageWidth / mImageCurrentWidth else mImageHeight / mImageCurrentHeight
             maxScale *= 2
         }
-        if (minScale != -1f && (mImageCurrentWidth != 0f && mImageCurrentHeight != 0f)){
-            minScale = if (mImageCurrentWidth /mImageWidth < mImageCurrentHeight / mImageHeight) mImageCurrentWidth /mImageWidth else mImageCurrentHeight / mImageHeight
+        if (minScale != -1f && (mImageCurrentWidth != 0f && mImageCurrentHeight != 0f)) {
+            minScale =
+                if (mImageCurrentWidth / mImageWidth < mImageCurrentHeight / mImageHeight) mImageCurrentWidth / mImageWidth else mImageCurrentHeight / mImageHeight
             minScale /= 2
         }
-        Log.e("区域大小与缩放范围","$mImageCurrentWidth    $mImageCurrentHeight     $minScale    $maxScale")
+        Log.e("区域大小与缩放范围", "$mImageCurrentWidth    $mImageCurrentHeight     $minScale    $maxScale")
 
-        mRect.left = mImageWidth*1f / 2 - mImageCurrentWidth*1f /2
+        mRect.left = mImageWidth * 1f / 2 - mImageCurrentWidth * 1f / 2
         mRect.right = mRect.left + mImageCurrentWidth
-        mRect.top = mImageHeight*1f / 2 - mImageCurrentHeight*1f / 2
+        mRect.top = mImageHeight * 1f / 2 - mImageCurrentHeight * 1f / 2
         mRect.bottom = mRect.top + mImageCurrentHeight
     }
+
+    private fun preDrawBitmap() {
+        val rect = blocks()
+        for (i in rect.left..rect.right) {
+            for (j in rect.top..rect.bottom) {
+
+            }
+        }
+    }
+
+    private fun blocks() = Rect(
+        floor(drawRect.left / blockSize), floor(drawRect.top / blockSize),
+        ceil(drawRect.right / blockSize), ceil(drawRect.bottom / blockSize)
+    )
+
+    private fun floor(value: Int) =
+        Math.floor(value.toDouble()).toInt()
+
+    private fun ceil(value: Int) =
+        Math.ceil(value.toDouble()).toInt()
 
 }
